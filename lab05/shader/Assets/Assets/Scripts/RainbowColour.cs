29ef385c695af5c52c0136243ee3a5c7b535b8b2
Drewtwo64 <96755965+Drewtwo64@users.noreply.github.com>
2023-09-28 22:09:12 -0500

    shader part 1

---
+++ lab05/shader/Assets/Assets/Scripts/RainbowColour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainbowColour : MonoBehaviour {
+
+	// References to render and material
+	Renderer rend;
+	Material material;
+
+	// Use this for initialization
+	void Start () {
+		// Get references,
+		rend = GetComponent<Renderer>(); // Get the renderer for this gameobject
+		material = rend.material; //Get the current material for the renderer
+
+		// Set shader value with name "_Colour" to Color.magenta
+		material.SetColor("_Colour", Color.magenta);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
